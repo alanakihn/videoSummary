f236abb3149128024a6a0ca52a5c5afe9408ec67
Alana Kihn <alana@10-249-215-215.wireless.oregonstate.edu>
2023-04-18 12:48:30 -0700

    npm transcript module added

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
-const express = require('express');
+import { YoutubeTranscript } from 'youtube-transcript';
+
+//const YoutubeTranscript = require('youtube-transcript');
+
+import express from 'express';
+
+//const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+
+
+app.use(express.json()); 
 app.use(express.static('homePage'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/homePage/home.html');
+});
+
+let summary = {
+    "summary": "This is a summary"
+}
+
+app.get('/api/summary', (req, res) => {
+    // talk to youtube api and get captions
+
+    // send back summary of the video
+    //res.send(summary);
+    YoutubeTranscript.fetchTranscript('https://www.youtube.com/watch?v=S46iJIk3t70&t=930s').then((transcript) => {
+        res.send(transcript);
+    });
 });
 
 // Start the server
